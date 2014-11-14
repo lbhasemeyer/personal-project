17ffc35b7643be33ce44aebaecf37a085d13d78c
lbhasemeyer <lbhasemeyer@gmail.com>
2014-11-14 16:31:28 -0700

    used the wc3 markup validation to remove a form in a form on the layout application page

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -6,8 +6,7 @@ feature "users" do
       visit root_path
       click_on "Users"
       click_on "Create User"
-      click_button "Add User"
-    save_and_open_page
+      click_on "Add User"
       expect(page).to have_content("First name can't be blank")
       expect(page).to have_content("Last name can't be blank")
       expect(page).to have_content("Email can't be blank")
@@ -16,6 +15,15 @@ feature "users" do
 
   scenario "User creates a user - passwords don't match" do
       visit root_path
+      click_on "Users"
+      click_on "Create User"
+      fill_in "First name", with: "Ron"
+      fill_in "Last name", with: "Burgundy"
+      fill_in "Email", with: "anchor@man.com"
+      fill_in "Password", with: "sandiego"
+      fill_in "Password confirmation", with: "whale"
+      click_on "Add User"
+      expect(page).to have_content("Password confirmation doesn't match Password")
   end
 
 end
