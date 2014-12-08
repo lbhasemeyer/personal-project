148bbc270eca10ef24034727a87c0eb1abddf70f
lbhasemeyer <lbhasemeyer@gmail.com>
2014-12-07 17:25:56 -0700

    added avatar field to new registration signup page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -18,7 +18,7 @@ class RegistrationsController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar)
   end
 
 end
