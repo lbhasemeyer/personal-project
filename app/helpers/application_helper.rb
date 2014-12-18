57d004b14eaf1a7facf4bd51835580c7ab724ff2
lbhasemeyer <lbhasemeyer@gmail.com>
2014-12-17 21:18:06 -0700

    added page title for tab to all pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  def page_title(title, &block)
+    content_for(:title, title)
+
+    html = '<div>'.html_safe
+
+    if block
+      html += '<div class ="pull-right">'.html_safe
+      html += capture do
+        block.call
+      end
+      html += '</div>'.html_safe
+    end
+
+    html += '<h1>'.html_safe
+    html += title
+    html += '</h1>'.html_safe
+    html += '</div>'.html_safe
+    html
+  end
+
 end
