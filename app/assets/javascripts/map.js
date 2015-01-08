243c31d0b7e9d1bce0ba5671b0aeb1e8568af73d
lkbhas <lbhasemeyer@gmail.com>
2015-01-08 10:49:04 -0700

    fixed javascript - countries select on one click, centered maps in css

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,39 +1,29 @@
 $(document).ready(function() {
 
-  var clickedCountries = []
+  var clickedCountries = [];
 
-  var changeCountryColor = function(code){
-    var toChange = {};
-    toChange[code] = 'orange';
-
-    jQuery('#vmap').vectorMap('set', 'colors', toChange);
-
-  };
-
-  $('#vmap').vectorMap(
-    {
-      map: 'world_en',
-      hoverColor: '#d3d3d3',
-      onRegionClick: function(element, code, region)
-      {
-        changeCountryColor(code);
-        clickedCountries.push(code);
-      },
-      // onRegionOver: function(event, code)
-      // {
-      //   if (clickedCountries.includes(code))
-      //     {
-      //       event.preventDefault();
-      //     }
-      // },
-      // onRegionOut: function(event, code)
-      // {
-      //   if (clickedCountries.includes(code))
-      //     {
-      //       event.preventDefault();
-      //     }
-      //   },
+  $('#vmap').vectorMap({
+    map: 'world_en',
+    hoverColor: '#d3d3d3',
+    selectedColor: 'orange',
+    onRegionClick: function(event, code, region) {
+      jQuery('#vmap').vectorMap('set', 'colors', code, 'orange');
+      clickedCountries.push(code);
     }
-  );
+    // onRegionOver: function(event, code)
+    // {
+    //   if (clickedCountries.includes(code))
+    //     {
+    //       event.preventDefault();
+    //     }
+    // },
+    // onRegionOut: function(event, code)
+    // {
+    //   if (clickedCountries.includes(code))
+    //     {
+    //       event.preventDefault();
+    //     }
+    //   },
+  });
 
 });
