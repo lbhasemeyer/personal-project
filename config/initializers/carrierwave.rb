8463669bd5255c583fa1b9d61aebc4b06c25aea3
lkbhas <lbhasemeyer@gmail.com>
2015-01-23 14:28:36 -0700

    adding travis

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,17 @@
 CarrierWave.configure do |config|
- config.fog_credentials = {
-   :provider               => 'AWS',                        # required
-   :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],                      # required
-   :aws_secret_access_key  => ENV["AWS_SECRET_KEY"],                        # required
-   :region                 => 'us-west-2'
- }
- config.fog_directory  = ENV["AWS_S3_BUCKET"]                          # required
- config.fog_public     = false                                        # optional, defaults to true
- config.fog_attributes = {'Cache-Control'=>"max-age=#{365.day.to_i}"} # optional, defaults to {}
+  if Rails.env.test?
+    config.storage = :file
+    config.enable_processing = false
+  else
+
+   config.fog_credentials = {
+     :provider               => 'AWS',                        # required
+     :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],                      # required
+     :aws_secret_access_key  => ENV["AWS_SECRET_KEY"],                        # required
+     :region                 => 'us-west-2'
+   }
+   config.fog_directory  = ENV["AWS_S3_BUCKET"]                          # required
+   config.fog_public     = false                                        # optional, defaults to true
+   config.fog_attributes = {'Cache-Control'=>"max-age=#{365.day.to_i}"} # optional, defaults to {}
+  end
 end
