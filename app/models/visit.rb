fee6f6048c2e7181c28c55e0fd325e7a997735bf
lkbhas <lbhasemeyer@gmail.com>
2015-02-19 13:01:49 -0700

    added dependent destroy to visits so countries will remove again

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -2,8 +2,8 @@ class Visit < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :country
-  has_many :blogs
-  has_many :comments
-  has_many :photos
-  
+  has_many :blogs, :dependent => :destroy
+  has_many :comments, :dependent => :destroy
+  has_many :photos, :dependent => :destroy
+
 end
